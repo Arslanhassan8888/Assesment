b15d8a04416cd7d7a9b5eb5a59c4c31b48c45744
Arslanhassan8888 <arslanarslan84@yahoo.com>
2024-11-15 13:11:51 +0000

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,26 @@
+def cels_to_fah(value):
+    result=(value*(9/5))+32
+    return result
+
+def fah_to_cels(value):
+    result=(value -32)*(5/9)
+    return result
+
+conversion= input("Please enter 'c' if you want to convert from Celsius to Fahrenheit or enter 'f' if you want to convert from Fahrenheit to Celsius: ").lower()
+
+if conversion == 'c':
+    temp_celsius = float(input("Please enter the temperature value: "))
+    value_in_fahrenheit = cels_to_fah(temp_celsius)
+    print(temp_celsius, "Celsius are", value_in_fahrenheit, "Farhenheit")
+
+
+elif conversion == 'f':
+    temp_fahrenheit = float(input("Please enter the temperature value: "))
+    value_in_celsius = fah_to_cels(temp_fahrenheit)
+    print(temp_fahrenheit, "Fahrenheit are", value_in_celsius, "Celsius")
+
+else:
+    print("Invalid entry, please enter 'c' or 'f'")
+
+                         
+    
