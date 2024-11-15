7100876f1edfc9a4074673513ed9a9eb0b26e4ae
Arslanhassan8888 <arslanarslan84@yahoo.com>
2024-11-15 13:12:17 +0000

    Add files via upload

---
+++ listops.py
@@ -0,0 +1,7 @@
+my_list = [10,20,30,40,50,70,80,90,100,110]
+print(my_list)
+print("The sum of the list is: ", sum(my_list))
+print("The largest number of the list is: ", max(my_list), "The smallest number of the list is: ", min(my_list))
+my_list.reverse()
+print("List in the reverse ordder: ", my_list)
+print("The largest number of the list is: ", max(my_list), "The smallest number of the list is: ", min(my_list))
