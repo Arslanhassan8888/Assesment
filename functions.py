602186b7e611456973fd31b6f73ad3b6daa73ca9
Arslanhassan8888 <arslanarslan84@yahoo.com>
2024-12-06 14:55:32 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,15 +9,20 @@ def fah_to_cels(value):
 conversion= input("Please enter 'c' if you want to convert from Celsius to Fahrenheit or enter 'f' if you want to convert from Fahrenheit to Celsius: ").lower()
 
 if conversion == 'c':
-    temp_celsius = float(input("Please enter the temperature value: "))
-    value_in_fahrenheit = cels_to_fah(temp_celsius)
-    print(temp_celsius, "Celsius are", value_in_fahrenheit, "Farhenheit")
-
+    try:
+        temp_celsius = float(input("Please enter the temperature value: "))
+        value_in_fahrenheit = cels_to_fah(temp_celsius)
+        print(temp_celsius, "Celsius are", value_in_fahrenheit, "Farhenheit")
+    except ValueError:
+        print("Invalid temperature value. Please enter a numeric value.")
 
 elif conversion == 'f':
-    temp_fahrenheit = float(input("Please enter the temperature value: "))
-    value_in_celsius = fah_to_cels(temp_fahrenheit)
-    print(temp_fahrenheit, "Fahrenheit are", value_in_celsius, "Celsius")
+    try:
+        temp_fahrenheit = float(input("Please enter the temperature value: "))
+        value_in_celsius = fah_to_cels(temp_fahrenheit)
+        print(temp_fahrenheit, "Fahrenheit are", value_in_celsius, "Celsius")
+    except ValueError:
+        print("Invalid temperature value. Please enter a numeric value.")
 
 else:
     print("Invalid entry, please enter 'c' or 'f'")
